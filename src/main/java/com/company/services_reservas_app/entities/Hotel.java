1a245465fbd0a560e2c9fa180604b19c37e37772
Diego Luz <dieguinholuz10@gmail.com>
2024-06-26 14:26:50 -0300

    Novas entidades Cliente e Endereço

--- src/main/java/com/company/services_reservas_app/entities/Hotel.java
+++ src/main/java/com/company/services_reservas_app/entities/Hotel.java
@@ -27,6 +27,98 @@ public class Hotel {
 
     private double precoPorNoite;
 
+    //Construtor padrão
+
+    public Hotel() {
+    }
+
+    //Construtor com os parâmetros
+
+    public Hotel(Long id, String nome, String endereco, String cidade, String estado, String pais, int numeroEstrelas, String descricao, double precoPorNoite) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+        this.numeroEstrelas = numeroEstrelas;
+        this.descricao = descricao;
+        this.precoPorNoite = precoPorNoite;
+    }
+
+
     // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getNumeroEstrelas() {
+        return numeroEstrelas;
+    }
+
+    public void setNumeroEstrelas(int numeroEstrelas) {
+        this.numeroEstrelas = numeroEstrelas;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPrecoPorNoite() {
+        return precoPorNoite;
+    }
+
+    public void setPrecoPorNoite(double precoPorNoite) {
+        this.precoPorNoite = precoPorNoite;
+    }
 }
 
